b2a40ff34aade513f3d43fbc6bd7481ac86acff1
Tobias Billep <tobias.billep@artcom-venture.de>
2022-01-12 19:00:42 +0100

    date data improvement

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import datetime, time
 import os
 import os.path
 import pickle
+import re
+
 import exiftool
 from pathlib import Path
 from shutil import copyfile
@@ -24,11 +26,10 @@ def get_exif_data(file_path):
         exifdata = et.get_metadata(file_path)
         return exifdata
 
-def get_folder_name(datestring, dest):
-    if not datestring:
+def get_folder_name(dt, dest):
+    if not dt:
         path = os.path.join(dest, "no_exif_data")
     else:
-        dt = datetime.datetime.strptime(datestring, "%Y:%m:%d %H:%M:%S")
         path = os.path.join(dest, str(dt.year), str(dt.month))
     Path(path).mkdir(parents=True, exist_ok=True)
     return path
@@ -45,9 +46,24 @@ def increment_file_dest(file_dest):
             return file_dest
 
 
+def determine_date_by_filename(file_path):
+    pattern = re.compile(r"IMG-(\d{8})-(WA\d+).jpg")
+    m = pattern.search(file_path)
+    if m is not None:
+        return datetime.datetime.strptime(m.group(1), "%Y%m%d")
+    return None
+
 def copy_foto(file_path, dest):
     exif_data = get_exif_data(file_path)
-    dt = exif_data['EXIF:DateTimeOriginal'] if "EXIF:DateTimeOriginal" in exif_data else None
+    print(exif_data)
+    if "EXIF:DateTimeOriginal" in exif_data:
+        dt = exif_data["EXIF:DateTimeOriginal"]
+        dt = datetime.datetime.strptime(dt, "%Y:%m:%d %H:%M:%S")
+    elif "QuickTime:MediaCreateDate" in exif_data:
+        dt = exif_data["QuickTime:MediaCreateDate"]
+        dt = datetime.datetime.strptime(dt, "%Y:%m:%d %H:%M:%S")
+    else:
+        dt = determine_date_by_filename(file_path)
     folder_name = get_folder_name(dt, dest)
     file_name = os.path.basename(file_path)
     file_dest = os.path.join(folder_name, file_name)
@@ -75,18 +91,17 @@ def get_cmd_parser():
 if __name__ == '__main__':
     fotos = load_foto_pickle(FOTOPICKLE)
     args = get_cmd_parser().parse_args()
-    i = 0
-    for f in find_files(args.srcpath, FILEEXT):
-        if not f in fotos:
-            i += 1
-            copy_foto(f, args.destpath)
-            fotos.append(f)
-
-        if i % 10 == 0:
-            print("save pickel {0}".format(FOTOPICKLE))
-            save_foto_pickle(fotos, FOTOPICKLE)
-        if i > 1000:
-            break
-        #copyfile(f)
-    print("save pickel {0}".format(FOTOPICKLE))
-    save_foto_pickle(fotos, FOTOPICKLE)
+    copy_foto(args.srcpath, args.destpath)
+    # i = 0
+    # for f in find_files(args.srcpath, FILEEXT):
+    #     if not f in fotos:
+    #         i = i +1 if copy_foto(f, args.destpath) else i
+    #         fotos.append(f)
+    #         if i % 10 == 0:
+    #             print("save pickel {0}".format(FOTOPICKLE))
+    #             save_foto_pickle(fotos, FOTOPICKLE)
+    #     if i > 1000:
+    #         break
+    #     #copyfile(f)
+    # print("save pickel {0}".format(FOTOPICKLE))
+    # save_foto_pickle(fotos, FOTOPICKLE)
